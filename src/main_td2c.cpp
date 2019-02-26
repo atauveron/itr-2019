4d9d57684cc1a366aa08d563bed494aacad9e825
Aimery Tauveron <atauveron@via.ecp.fr>
2019-02-26 09:10:07 +0100

    Question 2c - update 2a

--- src/main_td2c.cpp
+++ src/main_td2c.cpp
@@ -19,42 +19,58 @@ struct ThreadData {
   volatile unsigned int nLoops;
   /// The counter to increment
   volatile double counter;
+  /// Boolean to indicate whether to use the mutex
+  bool useMutex;
+  /// Mutex on the counter
+  pthread_mutex_t mutex;
 };
 
 /**
  * @brief A function to increment a counter
- * 
+ *
  * @param nLoops the number of loops to perform
  * @param[in,out] pCounter a pointer to the counter to increment
  */
-void incr(unsigned int nLoops, volatile double *pCounter) {
+void incr(unsigned int nLoops, volatile double *pCounter, bool useMutex,
+          pthread_mutex_t *pMutex) {
   for (unsigned int i(0); i < nLoops; ++i) {
+    if (useMutex) {
+      pthread_mutex_lock(pMutex);
+    }
     (*pCounter) += 1.0;
+    if (useMutex) {
+      pthread_mutex_unlock(pMutex);
+    }
   }
   return;
 }
 
 /**
  * @brief A wrapper to call `incr` in a pthread
- * 
+ *
  * @param v_data[in,out] a pointer to the data passed to the thread
  */
 void *call_incr(void *v_data) {
   ThreadData *p_data = reinterpret_cast<ThreadData *>(v_data);
-  incr(p_data->nLoops, &p_data->counter);
+  incr(p_data->nLoops, &p_data->counter, p_data->useMutex, &p_data->mutex);
   return v_data;
 }
 
 int main(int argc, char **argv) {
   if (argc < 3) {
-    std::cerr << "Usage: " << argv[0] << " <nLoops> <nTasks>\n";
+    std::cerr << "Usage: " << argv[0] << " <nLoops> <nTasks> [protected]\n";
     return 1;
   }
   unsigned int nLoops = std::stoul(argv[1]);
   unsigned int nTasks = std::stoul(argv[2]);
+  bool useMutex(false);
+  if (argc == 4 && argv[3] == "protected") {
+    useMutex = true;
+  }
 
   // Thread
-  ThreadData data{nLoops, 0};
+  ThreadData data{nLoops, 0, useMutex};
+  pthread_mutex_init(&data.mutex, nullptr);
   std::vector<pthread_t> incrementThreads(nTasks);
 
   // Time
@@ -66,6 +82,7 @@ int main(int argc, char **argv) {
   for (auto &thread : incrementThreads) {
     pthread_join(thread, nullptr);
   }
+  pthread_mutex_destroy(&data.mutex);
   clock_gettime(CLOCK_REALTIME, &end_ts);
   std::cout << "Execution time: " << timespec_to_ms(end_ts - start_ts) / 1000
             << "s\n";
