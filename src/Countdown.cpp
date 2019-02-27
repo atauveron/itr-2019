bf173a52692e42a1cc34a55669c1f4bd1b6c07a0
Shin <edmond.de-roffignac@student.ecp.fr>
2019-02-27 23:00:29 +0100

    Add Countdown implementation

---
+++ src/Countdown.cpp
@@ -0,0 +1,14 @@
+#include "Countdown.h"
+#include "PeriodicTimer.h"
+
+Countdown::Countdown(int n) : PeriodicTimer() {
+    counter = n;
+}
+
+void Countdown::callback() : {
+    --counter;
+
+    if (counter == 0) {
+        this.stop();
+    }
+}
---
+++ src/Countdown.h
@@ -0,0 +1,27 @@
+/**
+ * @file Countdown.h
+ * @brief Implementation of the Countdown class
+ */
+
+#ifndef COUNTDOWN_INCLUDED
+#define COUNTDOWN_INCLUDED
+
+#include <ctime>
+#include <csignal>
+
+/**
+ * @brief A class that implements a countdown
+ */
+class Countdown : PeriodicTimer {
+
+private:
+  int counter;
+
+public:
+  Countdown(int n)
+
+private:
+  void callback()
+};
+
+#endif
