83342f09f18955ab64fe35b2476be52e23f5fd1a
Shin <edmond.de-roffignac@student.ecp.fr>
2019-03-03 11:34:02 +0100

    Mutex header

---
+++ src/Mutex.h
@@ -0,0 +1,43 @@
+/**
+ * @file Mutex.h
+ * @brief Implementation of the Mutex class
+ */
+
+#ifndef MUTEX_INCLUDED
+#define MUTEX_INCLUDED
+
+#include <pthread.h>
+
+/**
+ * @brief A class that implements a mutex
+ */
+class Mutex {
+
+	public:
+		class Lock {
+			public:
+				Lock(Mutex &m);
+				Lock(Mutex &m, double timeout_ms);
+				~Lock();
+				void wait();
+				bool wait(double timeout_ms);
+				void notify();
+				void notifyAll();
+		};
+
+	private:
+		pthread_mutex_t posixId;
+		pthread_cond_t posixCondId;
+
+	public:
+		Mutex();
+		~Mutex();
+
+	private:
+		void lock();
+		bool lock(double timeout_ms);
+		bool trylock();
+		bool unlock();
+};
+
+#endif
