98fa6345753aab7e6ba0f8d069c1833b01366d6d
Aimery Tauveron <atauveron@via.ecp.fr>
2019-03-12 10:45:42 +0100

    Fix Thread.cpp

--- src/Thread.cpp
+++ src/Thread.cpp
@@ -10,11 +10,11 @@ void Thread::start() { call_run(this); }
 
 // DOES NOT USE A THREAD !!!!
 void *Thread::call_run(void *v_thread) {
-  Thread *thread = reinterpret_cast<Thread *>(v_thread);
-  thread->chrono.restart();
-  thread->run();
-  thread->join(); /// NOTHING TO JOIN
-  thread->chrono.stop;
+	Thread *thread = reinterpret_cast<Thread *>(v_thread);
+	thread->chrono.restart();
+	thread->run();
+	thread->join(); // NOTHING TO JOIN
+	thread->chrono.stop();
 }
 
 void sleep_ms(double delay_ms) { timespec_wait(timespec_from_ms(delay_ms)); }
