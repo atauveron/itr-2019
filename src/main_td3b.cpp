8d3316c5864ea86c7d54631ee7da549183ef28b6
Aimery Tauveron <atauveron@via.ecp.fr>
2019-03-12 10:40:40 +0100

    Fix main_td3b.cpp

--- src/main_td3b.cpp
+++ src/main_td3b.cpp
@@ -1,8 +1,12 @@
 #include "Countdown.h"
 
-int main (int argc, char **argv) {
-  Countdown countdown(10);
-  countdown.start(1000);
+#include <unistd.h>
 
-  return 0;
+int main(int argc, char **argv) {
+	Countdown countdown(10);
+	countdown.start(1000);
+
+	sleep(20000);
+
+	return 0;
 }
