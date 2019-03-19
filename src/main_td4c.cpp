2d44d1eb8a0cf0ac0b37888aad90c42c3cd1e4fa
Aimery Tauveron <atauveron@via.ecp.fr>
2019-03-19 10:21:12 +0100

    Fix TD4c

--- src/main_td4c.cpp
+++ src/main_td4c.cpp
@@ -21,8 +21,8 @@ int main(int argc, char **argv) {
 	std::vector<std::thread> consThreads{};
 	std::vector<unsigned int> nTaken(nCons, 0);
 	std::vector<std::thread> prodThreads{};
-	for (auto k = 0; k < nCons; ++k) {
-		std::thread tmp(
+	for (unsigned int k = 0; k < nCons; ++k) {
+		consThreads.push_back(std::thread(
 				[](Semaphore &sem, unsigned int &n) {
 					bool success(true);
 					while (success) {
@@ -32,16 +32,16 @@ int main(int argc, char **argv) {
 						}
 					}
 				},
-				std::ref(semaphore), std::ref(nTaken.at(k)));
-		consThreads.push_back(tmp);
+				std::ref(semaphore), std::ref(nTaken.at(k))));
 	}
-	for (auto k = 0; k < nProd; ++k) {
-		std::thread tmp([&nTokens](Semaphore &sem) {
-			for (auto k = 0; k < nTokens; ++k) {
-				sem.give();
-			}
-		});
-		prodThreads.push_back(tmp);
+	for (unsigned int k = 0; k < nProd; ++k) {
+		prodThreads.push_back(std::thread(
+				[&nTokens](Semaphore &sem) {
+					for (unsigned int k = 0; k < nTokens; ++k) {
+						sem.give();
+					}
+				},
+				std::ref(semaphore)));
 	}
 
 	// Join the threads
