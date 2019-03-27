cca73786a054895b095492c37254b0a09527382d
Shin <edmond.de-roffignac@student.ecp.fr>
2019-03-27 16:00:15 +0100

    Update timespec test

--- src/main_td1e.cpp
+++ src/main_td1e.cpp
@@ -10,12 +10,20 @@
 #include "timespec.h"
 
 int main() {
-	timespec time1_ts{.tv_sec = 5, .tv_nsec = 0};
-	timespec time2_ts{.tv_sec = 5, .tv_nsec = 500000000};
-	timespec res = timespec_add(time1_ts, time2_ts);
-	std::cout << res.tv_sec << "s, " << res.tv_nsec << "ns\n";
-	std::cout << (time1_ts < time2_ts) << '\n';
+    timespec time1_ts = timespec_from_ms(5000);
+    timespec time2_ts = timespec_from_ms(5500);
+    timespec now = timespec_now();
 
-	// Return
-	return 0;
+    timespec ressum = time1_ts + time2_ts;
+    timespec resdiff = time1_ts - time2_ts;
+    std::cout << "SUM: " << timespec_to_ms(ressum) << "ms | " << ressum.tv_sec
+	      << "s and " << ressum.tv_nsec << "ns\n";
+    std::cout << "DIFF: " << timespec_to_ms(resdiff) << "ms | "
+	      << resdiff.tv_sec << "s and " << resdiff.tv_nsec << "ns\n";
+    std::cout << "NOW: " << timespec_to_ms(now) << "ms | " << now.tv_sec
+	      << "s and " << now.tv_nsec << "ns\n";
+    std::cout << (time1_ts < time2_ts) << '\n';
+
+    // Return
+    return 0;
 }
