9e17cd942b31b24be8e163e1afe45cd79221b638
Aimery Tauveron <atauveron@via.ecp.fr>
2019-03-28 14:50:59 +0100

    Update report

--- src/main_td3c.cpp
+++ src/main_td3c.cpp
@@ -1,18 +1,19 @@
+#include <iostream>
+
 #include "Calibrator.h"
 #include "Chrono.h"
 #include "CpuLoop.h"
-#include <iostream>
 
 int main() {
-    Calibrator callibrator(1000, 10);
-    CpuLoop loop(callibrator);
+	Calibrator calibrator(1000, 10);
+	CpuLoop loop(calibrator);
 
-    std::cout << "Running for 10 sec" << std::endl;
-    Chrono chrono;
-    loop.runTime(10000);
-    chrono.stop();
+	std::cout << "Running for 10 sec" << std::endl;
+	Chrono chrono;
+	loop.runTime(10000);
+	chrono.stop();
 
-    std::cout << "Result in ms: " << chrono.lap() << std::endl;
+	std::cout << "Result in ms: " << chrono.lap() << std::endl;
 
-    return 0;
+	return 0;
 }
